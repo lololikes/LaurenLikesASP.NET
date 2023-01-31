befb67280679e19230605d12bb73aae9bf9ede0b
lololikes8 <laurenlikes@gmail.com>
2023-01-30 22:31:20 -0700

    Add project files.

---
+++ FirstEmpty/wwwroot/js/FanMail.js
@@ -0,0 +1,9 @@
+﻿// j query way of doing javascript
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() 
+        + ", Subject: " + $("#txtSubject").val()
+        + ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeOut("slow");
+})
+
